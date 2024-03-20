d952b3d483d840ce3587ec872e169126d192fc5e
Nate Riffe <inkblot@movealong.org>
2024-03-20 18:44:12 -0500

    Implement an app wiring framework
    Create a framework for putting an application together functionally. Uses
    bindings to create a dictionary of services. Services can depend on other
    services. The built application is represented as a function and run via a
    monad transformation.

---
+++ app/src/main/java/org/movealong/sly/app/ServiceHandle.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2024 Nate Riffe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.movealong.sly.app;
+
+import com.jnape.palatable.lambda.adt.hmap.TypeSafeKey;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import static java.lang.Thread.currentThread;
+import static lombok.AccessLevel.PRIVATE;
+
+/**
+ * A handle with non-fungible values that is used for binding {@link Service}s
+ * in an {@link App}. Each {@link Service} that is bound in an {@link App}
+ * should have its own <code>ServiceHandle</code> that other {@link Service}s
+ * can use to wire the associated {@link Service}.
+ *
+ * @param <S> the service type
+ */
+@ToString
+@RequiredArgsConstructor(access = PRIVATE)
+public final class ServiceHandle<S> implements TypeSafeKey.Simple<Service<S>> {
+    private final StackTraceElement initializedAt;
+
+    public static <S> ServiceHandle<S> create() {
+        return new ServiceHandle<>(currentThread().getStackTrace()[2]);
+    }
+}
---
+++ app/src/test/java/org/movealong/sly/app/AppTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2024 Nate Riffe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.movealong.sly.app;
+
+import com.jnape.palatable.lambda.io.IO;
+import org.junit.jupiter.api.Test;
+
+import static com.jnape.palatable.lambda.io.IO.io;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.movealong.sly.app.App.*;
+import static org.movealong.sly.app.Application.application;
+import static org.movealong.sly.app.Service.service;
+import static org.movealong.sly.app.ServiceHandle.create;
+import static testsupport.matchers.IOMatcher.yieldsValue;
+
+class AppTest {
+
+    @Test
+    void runsAService() {
+        ServiceHandle<Application<String, IO<?>, IO<String>>> handle = create();
+
+        assertThat(run(bind(handle, service(io(application(() -> io("service")))))
+                           .andThen(resolve(handle))),
+                   yieldsValue(equalTo("service")));
+    }
+
+    @Test
+    void runsAServiceWithDependencies() {
+        ServiceHandle<String>                                 aKey           = create();
+        ServiceHandle<String>                                 bKey           = create();
+        ServiceHandle<Application<String, IO<?>, IO<String>>> producerHandle = create();
+
+        assertThat(run(bind(aKey, service(io("composed")))
+                           .andThen(bind(bKey, service(io("service"))))
+                           .andThen(bind(producerHandle, service(
+                               aKey, bKey, (a, b) -> service(io(application(() -> io(a + " " + b)))))))
+                           .andThen(resolve(producerHandle))),
+                   yieldsValue(equalTo("composed service")));
+    }
+
+    @Test
+    void detectsDuplicateBindings() {
+        ServiceHandle<String>                                 key            = create();
+        ServiceHandle<Application<String, IO<?>, IO<String>>> producerHandle = create();
+
+        assertThrows(ServiceException.class,
+                     () -> run(bind(key, service(io("first")))
+                                   .andThen(bind(key, service(io("second"))))
+                                   .andThen(bind(producerHandle, service(
+                                       key, s -> service(io(application(() -> io("resolved: %" + s)))))))
+                                   .andThen(resolve(producerHandle))));
+    }
+
+    @Test
+    void detectsResolutionCycles() {
+        ServiceHandle<String>                                 aKey           = create();
+        ServiceHandle<String>                                 bKey           = create();
+        ServiceHandle<Application<String, IO<?>, IO<String>>> producerHandle = create();
+
+        assertThrows(ServiceException.class,
+                     () -> run(bind(aKey, service(bKey, b -> service(io("service A depends on " + b))))
+                                   .andThen(bind(bKey, service(
+                                       aKey, a -> service(io("service B depends on " + a)))))
+                                   .andThen(bind(producerHandle, service(
+                                       aKey, a -> service(io(application(() -> io("resolved: " + a)))))))
+                                   .andThen(resolve(producerHandle))));
+    }
+
+    @Test
+    void detectsMissingDependency() {
+        ServiceHandle<String>                                 aKey           = create();
+        ServiceHandle<String>                                 bKey           = create();
+        ServiceHandle<Application<String, IO<?>, IO<String>>> producerHandle = create();
+        assertThrows(ServiceException.class,
+                     () -> run(bind(aKey, service(io("composed")))
+                                   .andThen(bind(producerHandle, service(
+                                       aKey, bKey, (a, b) -> service(io(application(() -> io(a + " " + b)))))))
+                                   .andThen(resolve(producerHandle))));
+    }
+}
